11f75226fba3d3f4e838e121b3b58c0ce0c10e4b
Kajal <kajal.singh@olx.in>
2024-12-23 12:26:11 +0530

    Initial Commit

---
+++ Movie Tracker/Movie_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_TrackerApp.swift
+//  Movie Tracker
+//
+//  Created by Kajal on 23/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Movie_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Movie TrackerTests/Movie_TrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_TrackerTests.swift
+//  Movie TrackerTests
+//
+//  Created by Kajal on 23/12/24.
+//
+
+import Testing
+@testable import Movie_Tracker
+
+struct Movie_TrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
